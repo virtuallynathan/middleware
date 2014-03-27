c014f42ff1263166c5b2094813077e43857eb9f1
Nathan Owens <virtuallynathan@evoboxes.org>
2014-03-27 13:53:29 +0000

    created add device api (basic)

--- src/github.com/virtuallynathan/middleware/middleware.go
+++ src/github.com/virtuallynathan/middleware/middleware.go
@@ -11,21 +11,81 @@ func main() {
 		EnableRelaxedContentType: true,
 	}
 	handler.SetRoutes(
-		rest.Route{"GET", "/countries", GetAllCountries},
-		rest.Route{"POST", "/countries", PostCountry},
-		rest.Route{"GET", "/countries/:code", GetCountry},
-		rest.Route{"DELETE", "/countries/:code", DeleteCountry},
+		rest.Route{"POST", "/device/add", AddDevice},
+		//rest.Route{"GET", "/device/:id", GetDeviceById},
+		//rest.Route{"POST", "/device/sensor", GetDeviceBySensorType},
+		///rest.Route{"GET", "/device/:location", GetDeviceByLocation},
+		//rest.Route{"DELETE", "/device/:id", RemoveDevice}
 	)
 	http.ListenAndServe(":8080", &handler)
 }
 
-type Country struct {
-	Code string
-	Name string
+type Device struct {
+	DeviceId        string
+	IpAddr          string
+	ListenPort      string
+	DeviceType      string
+	Location        string
+	ConnectionLimit string
+	//sensors         []string
 }
 
-var store = map[string]*Country{}
+var store = map[string]*Device{}
 
+func GetDeviceById(w *rest.ResponseWriter, r *rest.Request) {
+
+}
+
+func GetDeviceBySensorType(w *rest.ResponseWriter, r *rest.Request) {
+
+}
+
+func GetDeviceByLocation(w *rest.ResponseWriter, r *rest.Request) {
+
+}
+
+func AddDevice(w *rest.ResponseWriter, r *rest.Request) {
+	device := Device{}
+	err := r.DecodeJsonPayload(&device)
+	if err != nil {
+		rest.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if device.DeviceId == "" {
+		rest.Error(w, "device id required", 400)
+		return
+	}
+	if device.IpAddr == "" {
+		rest.Error(w, "device ipAddr required", 400)
+		return
+	}
+	if device.ListenPort == "" {
+		rest.Error(w, "device listenPort required", 400)
+		return
+	}
+	if device.DeviceType == "" {
+		rest.Error(w, "device type required", 400)
+		return
+	}
+	if device.Location == "" {
+		rest.Error(w, "device location required", 400)
+		return
+	}
+	if device.ConnectionLimit == "" {
+		rest.Error(w, "device connectionLimit required", 400)
+		return
+	}
+	store[device.DeviceId] = &device
+	w.WriteJson(&device)
+
+}
+
+func RemoveDevice(w *rest.ResponseWriter, r *rest.Request) {
+
+}
+
+//demo code to figure out what i'm doing
+/*
 func GetCountry(w *rest.ResponseWriter, r *rest.Request) {
 	code := r.PathParam("code")
 	country := store[code]
@@ -69,3 +129,4 @@ func DeleteCountry(w *rest.ResponseWriter, r *rest.Request) {
 	code := r.PathParam("code")
 	delete(store, code)
 }
+*/
