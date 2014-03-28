7f58000b16bd4824fb9e7e38acc174c1cb95b71a
Emily Dick <eld3@st-andrews.ac.uk>
2014-03-28 15:21:11 +0000

    Producer added
    Source code added

---
+++ CT/src/Producer/RandomProducerData.java
@@ -0,0 +1,59 @@
+package Producer;
+import java.util.Random;
+
+
+/**Class to produce random data for producer state
+ *
+ */
+public class RandomProducerData {
+	
+	//manifest constants
+	int MAX_NUMBER_LOCATIONS = 10;
+	int MAX_NUMBER_CONNECTIONS = 10;
+		
+	/**Fill all state with random values
+	 * @param p
+	 * @return
+	 */
+	public Producer setAllRandom(Producer p){		
+		p = randomLocation(p);
+		p = randomConnections(p);
+		p = randomSensors(p);		
+		return p;
+	}	
+	
+	/**Set a random location
+	 * @param p
+	 * @return
+	 */
+	public Producer randomLocation(Producer p){
+		Random rand = new Random();		
+		p.location = rand.nextInt(MAX_NUMBER_LOCATIONS-1);
+		return p;
+	}
+	
+	/**Set a random connection limit 
+	 * @param p
+	 * @return
+	 */
+	public Producer randomConnections(Producer p){
+		Random rand = new Random();		
+		p.location = rand.nextInt(MAX_NUMBER_CONNECTIONS-1);
+		return p;
+	}
+	
+	/**Set Random sensor values
+	 * @param p
+	 * @return
+	 */
+	public Producer randomSensors(Producer p){
+		Random rand = new Random();
+		p.accelerometer = rand.nextBoolean();
+		p.gps = rand.nextBoolean();
+		p.gravity = rand.nextBoolean();
+		p.temperature = rand.nextBoolean();
+		p.orientation = rand.nextBoolean();		
+		return p;
+	}
+
+}
