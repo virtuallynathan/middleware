3066925095065cca6a7ed2f4a7ee1c3fea19ebdd
Emily Dick <eld3@st-andrews.ac.uk>
2014-03-29 12:37:33 +0000

    Register Producer
    Added method stubs to register producers and send regular heartbeats.

---
+++ CT/src/Producer/ProducerRegister.java
@@ -0,0 +1,19 @@
+package Producer;
+
+public class ProducerRegister {
+	
+	/**Initial Registration of a Producer with the database.
+	 * Using API method POST device/add
+	 */
+	public void registerProducer(Producer p){
+		
+	}
+	
+	/**Method to send heartbeat to the register
+	 * @param p
+	 */
+	public void producerHeartBeat(Producer p){
+		
+	}
+
+}
