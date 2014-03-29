8bb48281771be044efb131c8d055b8b15bb8bb2c
Nathan Owens <virtuallynathan@evoboxes.org>
2014-03-29 11:33:41 +0000

    rearrange main so it doesnt die

--- src/github.com/virtuallynathan/middleware/middleware.go
+++ src/github.com/virtuallynathan/middleware/middleware.go
@@ -11,6 +11,19 @@ import (
 
 func main() {
 
+	//Begin database conneciton setup
+	db, err := sql.Open("mysql", "root:compmgmt123@tcp(127.0.0.1:3306)/middleware")
+	if err != nil {
+		fmt.Printf("error, could not open sql connection")
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Printf("error, could not connect to database.")
+	}
+
+	//Begin HTTP handling
 	handler := rest.ResourceHandler{
 		EnableRelaxedContentType: true,
 	}
@@ -23,16 +36,6 @@ func main() {
 	)
 	http.ListenAndServe(":8080", &handler)
 
-	db, err := sql.Open("mysql", "root:compmgmt123@tcp(127.0.0.1:3306)/middleware")
-	if err != nil {
-		fmt.Printf("error, could not open sql connection")
-	}
-	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		fmt.Printf("error, could not connect to database.")
-	}
 }
 
 //The struct of type Device stores all the information about a single device.
