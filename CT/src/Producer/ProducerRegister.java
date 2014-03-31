75be04def6b887696b1a7e8a4146f9b3f504b395
eld3 <eld3@pc2-033-l.cs.st-andrews.ac.uk>
2014-03-31 11:05:40 +0100

    Added response code testing

--- CT/src/Producer/ProducerRegister.java
+++ CT/src/Producer/ProducerRegister.java
@@ -10,46 +10,48 @@ import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
 
 public class ProducerRegister {
-	
-	
+
+
 	/**Initial Registration of a Producer with the database.
 	 * Using API method POST device/add
 	 */
 	public void registerProducer(Producer p){
-			
+
 		HttpClient hc = HttpClients.createDefault();
 		HttpPost httppost = new HttpPost("http://middleware.nathan.io:8080/device/add");		
 		try{			
 			//set parameters as JSON
 			String jsonstring = p.createJsonNoId();
 			StringEntity params = new StringEntity(jsonstring);
-	        httppost.setEntity(params);	
+			httppost.setEntity(params);	
 			//Execute and get the response.
 			HttpResponse response = hc.execute(httppost);
 			HttpEntity entity = response.getEntity();
-			
+
 			//tests if response is null, if not sets device id
 			if(entity!=null){
-				//test repsonse ok --todo /////////////////////////////////////////////////////
-				System.out.println(response.toString());
-				String r = EntityUtils.toString(entity);
-				JSONObject json = new JSONObject(r);
-				String id = (String) json.get("DeviceID");
-				p.setDeviceId(id);				
+				int code = response.getStatusLine().getStatusCode();
+				if(code == 200){
+					System.out.println(response.toString());
+					String r = EntityUtils.toString(entity);
+					JSONObject json = new JSONObject(r);
+					String id = (String) json.get("DeviceID");
+					p.setDeviceId(id);
+				}
 			}	
 		}catch(Exception e){
 			System.out.println("Failed to post using /device/add");
 			e.printStackTrace();
 		}
 	}
-	
-	
-	
+
+
+
 	/**Method to send heart beat to the register
 	 * @param p
 	 */
 	public void producerHeartBeat(Producer p){
-		
+
 	}
 
 }
